59add6ee0d776587e4f752497129dad8d2ce9b2e
EC2 Default User <watanabe.t.job@gmail.com>
2022-03-17 05:49:23 +0000

    [add]favorites_model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
 
   #attachment :profile_image
   belongs_to :user
+  has_many :fovorites, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books,only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :books,only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:edit, :update, :show, :index]
 
 end
