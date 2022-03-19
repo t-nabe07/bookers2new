455123dda85d24255b50afec110586b4b58a5f8a
EC2 Default User <watanabe.t.job@gmail.com>
2022-03-19 03:41:47 +0000

    [add]relationship_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,31 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books, dependent: :destroy
-  has_many :favorites, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  attachment :profile_image
 
   validates :name, presence: true,length: {minimum: 2, maximum: 20 },uniqueness: true
   validates :introduction, presence: false, length: {maximum: 50}
+
+
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id",dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id",dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed_id
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  attachment :profile_image
+
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
@@ -7,6 +9,10 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
-  resources :users, only: [:edit, :update, :show, :index]
+  resources :users, only: [:edit, :update, :show, :index] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings',as: 'followings'
+    get 'followers' => 'relationships#followers',as: 'followers'
+  end
 
 end
