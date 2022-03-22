52ac5bb76a63035c2bbdbc3040cecabd6583889c
EC2 Default User <watanabe.t.job@gmail.com>
2022-03-22 06:26:15 +0000

    [add]favorites_ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,21 @@
 class FavoritesController < ApplicationController
 
   def create
-    @book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
-    @book_favorite.save
-    redirect_to request.referer
+    #@book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: params[:book_id])
+    @favorite.save
+    #@book_favorite.save
+    #redirect_to request.referer
   end
 
   def destroy
-    @book_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
-    @book_favorite.destroy
-    redirect_to request.referer
+    #@book_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: params[:book_id])
+    @favorite.destroy
+    #@book_favorite.destroy
+    #redirect_to request.referer
   end
 
 end
